156dc6bbbf65703471f8ef9c5266a2e19a98d50f
Vitalii <v.movchan1995@gmail.com>
2021-04-15 22:19:12 +0300

    Created new C# .Net Core 5.0 Project

---
+++ Module-one/Module-one/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Module_one
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
